8a46ac8603710a0f4b9880344e16b9a7a0cddf8e
MARIA EDUARDA ROSA DA COSTA <maria.costa06@cs.up.edu.br>
2025-08-29 11:33:32 -0300

    API - Exemplos de Requisições

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,15 @@
+Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Funcionalidades
+//Requisições
+// - Endereço/URL
+// - Método HTTP
+app.MapGet("/", () => "Se você mudar a maneira como olha para as coisas, as coisas para as quais você olha mudam!");
+
+app.MapGet("/funcionalidade", () => "If you change the way you look at things, the things you look at change!");
+
+app.MapPost("/funcionalidade", () => "Webner Dear");
 
 app.Run();
