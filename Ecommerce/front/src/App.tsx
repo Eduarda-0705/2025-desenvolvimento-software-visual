e233ef7230eaee23d46c4d9fd511b0e23ae3ea6c
MARIA EDUARDA ROSA DA COSTA <maria.costa06@cs.up.edu.br>
2025-10-24 11:27:29 -0300

    Criação do Formulário/Front

--- Ecommerce/front/src/App.tsx
+++ Ecommerce/front/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ListarProdutos from './ListarProdutos';
+import ListarProdutos from './componentes/pages/produto/ListarProdutos';
+import CadastrarProduto from './componentes/pages/produto/CadastrarProduto';
 
 //Componentes
 // - HTML, CSS e JS ou TS
 function App() {
   return (
     <div id="api">
-      <h1>Minha primeira aplicação em React!</h1>
+      {/* <h1>Minha primeira aplicação em React!</h1> */}
       <ListarProdutos/>
+      <CadastrarProduto/>
     </div>
   );
 }
---
+++ Ecommerce/front/src/componentes/pages/produto/CadastrarProduto.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Produto from "../../../models/Produto";
+
+function CadastrarProduto(){
+
+    const[nome, setNome] = useState("");
+
+    function enviarProduto(event : any){
+        event.preventDefault();
+        submeterProdutoAPI();
+    }
+
+    async function submeterProdutoAPI() {
+        //Biblioteca AXIOS
+
+        const produto : Produto = {
+            nome : nome,
+            descricao : "Teste",
+            preco : 12345,
+            quantidade : 50
+        };
+
+        const resposta =
+        await fetch("http://localhost:5244/api/produto/cadastrar",{
+            method : "POST", 
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(produto)
+        });
+        
+    }
+
+    function escreverNome(event : any){
+        setNome(event.target.value);
+    }
+    return(
+        <div>
+            <h1>Cadastrar Produtos</h1>
+            <form onSubmit={enviarProduto}>
+                <div>
+                    <label>Nome:</label>
+                    <input onChange={escreverNome} type="text" />
+                 </div>
+                     <div>
+                    <label>Descrição:</label>
+                    <input type="text" />
+                 </div>
+                    <div>
+                        <button type="submit">Cadastrar</button>
+                    </div>
+            </form>
+        </div>
+    );
+}
+
+export default CadastrarProduto;
